288e05fa7c9d262f3a8642a9597cbc93998cec8d
Kilian Mader <Kilian.Mader@hotmail.de>
2022-11-15 13:56:25 +0100

    added visibility

--- Aufgabe02_03.c
+++ Aufgabe02_03.c
@@ -2,16 +2,16 @@
 int main()
 {
 
-float zahl1, zahl2;
+	float zahl1, zahl2;
 
-printf("Geben Sie die 1. Zahl ein: ");
-scanf("%f", &zahl1);
+	printf("Geben Sie die 1. Zahl ein: ");
+	scanf("%f", &zahl1);
 
-printf("Geben Sie die 2. Zahl ein: ");
-scanf("%f", &zahl2);
+	printf("Geben Sie die 2. Zahl ein: ");
+	scanf("%f", &zahl2);
 
-printf("Die Summe ergibt %.2f\n", zahl1 + zahl2);
+	printf("Die Summe ergibt %.2f\n\n", zahl1 + zahl2);
 
-return 0;
+	return 0;
 
 }
