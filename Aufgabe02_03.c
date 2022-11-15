71acebd737385dfb31f69ad28d7b6c3d620a62ed
Kilian Mader <Kilian.Mader@hotmail.de>
2022-11-15 13:50:33 +0100

    added Aufgabe02_03 and Aufgabe02_03.c

---
+++ Aufgabe02_03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+float zahl1, zahl2;
+
+printf("Geben Sie die 1. Zahl ein: ");
+scanf("%f", &zahl1);
+
+printf("Geben Sie die 2. Zahl ein: ");
+scanf("%f", &zahl2);
+
+printf("Die Summe ergibt %.2f\n", zahl1 + zahl2);
+
+return 0;
+
+}
